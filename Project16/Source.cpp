e1dda85353ae67bb76f3a0a0ef066a3438324b29
4vanyek <z232566755z@gmail.com>
2024-04-05 19:37:01 +0300

    add additional line

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main() {
 	cout << "random program for a c++ test\ngithub: https://github.com/mystieneko/403testrepo";
+	cout << "\nthis line was added in a commit named 'add additional line'";
 }
